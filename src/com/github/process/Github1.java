63c797efd0fd5459625d8782f378877f808bc4c4
91970 <munde.murli93@gmail.com>
2022-02-21 02:11:18 +0530

    "Hi i'm Murli This is my First Commit "

---
+++ src/com/github/process/Github1.java
@@ -0,0 +1,14 @@
+package com.github.process;
+
+public class Github1 {
+
+	public static void main(String[] args) {
+	//each progrmmer has his own branch 
+	//while Pushing code- individual must push code in his own branch
+   // while pulling code - individual must  pull/clone code from main or master branch only 
+  //every day before starting work we have to pull code from main/master branch 
+  // at the end of every day individual must have to push his own code in his own repository  or branch 
+
+	}
+
+}
